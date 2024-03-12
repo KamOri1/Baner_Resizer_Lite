ad051fb39920b61979782feaa0a89f6fdd032fd3
KamOri1 <142267945+KamOri1@users.noreply.github.com>
2024-03-12 15:09:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-import APS_API as aps
 from front import Front_app
 
-
-import os
 fr = Front_app()
-# catDir = fr.baner_directory()
-# data = 'test'
-
-
-fr.window_open()
-# aps.psBannerResizer(catDir, data)
-
-
-
-# def sel():
-#    selection = "You selected the option " + str(var.get())
-#    label.config(text = selection)
