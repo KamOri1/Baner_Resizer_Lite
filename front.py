28ea7ee65c1b6323bfa88f29b5c4f6cbbbc31321
KamOri1 <142267945+KamOri1@users.noreply.github.com>
2024-03-27 21:12:42 +0100

    Update front.py

--- front.py
+++ front.py
@@ -1,32 +1,59 @@
 import tkinter as tk
-import customtkinter
+from tkinter import ttk
+import customtkinter as ctk
 from tkinter import filedialog as fd
 import APS_API as aps
 import os
+#import serverConnection as sC
+#import pngTowebp as ptw
 
-
-class Front_app:
+class App(ctk.CTk):
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.geometry('400x400')
+        #main setup
+        super().__init__()
+        self.title("Banner Resizer")
+        self.geometry('500x300')
+        self.resizable(False, False)
 
-        self.label1 = tk.Label(text='Podaj date kampani YYYY/MM/DD: ')
-        self.dateforCatalog_ = tk.Entry()
-        self.label1.grid(row=1, column=0)
-        self.dateforCatalog_.grid(row=1, column=1)
+        # widgets
+        self.create_widgets()
+        self.create_layout()
+        # run
+        self.mainloop()
+
+
+
+
+
+
+    def create_widgets(self):
+        self.label1 = ctk.CTkLabel(master=self, text='Campaign date YYYYMMDD: ', font=('Open Sans', 16))
+        self.dateforCatalog_ = ctk.CTkTextbox(master=self, corner_radius=5, border_color='green', border_width=1,
+                                              width=200, height=10)
         self.var = tk.IntVar()
         self.var.trace_add("write", self.update_cam_data)
-        self.R1 = tk.Radiobutton(self.window, text="610x242", variable=self.var, value=610)
-        self.R1.grid(row=2, column=0)
-        self.R2 = tk.Radiobutton(self.window, text="650x490", variable=self.var, value=650)
-        self.R2.grid(row=2, column=1)
-        self.button_1 = tk.Button(text='Select folder', width=20, bg="green", fg="yellow", command=self.get_banner_dir)
-        self.button_1.grid(row=3, column=0)
-        self.button_2 = tk.Button(text='Resize a Banner', width=20, bg="green", fg="yellow", command=self.resize_banner)
-        self.button_2.grid(row=3, column=1)
-        self.window.mainloop()
+        self.R1 = ctk.CTkRadioButton(master=self, text="610x242", variable=self.var, value=610, hover_color='#d11507',
+                                     radiobutton_width=15, radiobutton_height=15, font=('Open Sans', 14))
+        self.R2 = ctk.CTkRadioButton(master=self, text="650x490", variable=self.var, value=650, hover_color='#d11507',
+                                     radiobutton_width=15, radiobutton_height=15, font=('Open Sans', 14))
+        self.button_1 = ctk.CTkButton(master=self, text='Select folder', width=100, fg_color="green", hover_color='#34661e',
+                                      font=('Open Sans', 14), command=self.get_banner_dir)
+        self.button_2 = ctk.CTkButton(master=self, text='Resize', width=100, fg_color="green", hover_color='#34661e',
+                                      font=('Open Sans', 14), command=self.resize_banner)
+        self.button_3 = ctk.CTkButton(master=self, text='Exit', width=100, fg_color="red", hover_color='#d11507',
+                                      font=('Open Sans', 14), command=self.quite_app)
+
         self.ban_dir = None
         self.baner_size_data = None
+
+    def create_layout(self):
+        self.label1.place(x=120, y=40, anchor='center')
+        self.dateforCatalog_.place(x=350, y=40, anchor='center')
+        self.R1.place(x=68, y=80, anchor='center')
+        self.R2.place(x=160, y=80, anchor='center')
+        self.button_1.place(x=60, y=120, anchor='center')
+        self.button_2.place(x=60, y=160, anchor='center')
+        self.button_3.place(x=60, y=200, anchor='center')
 
     def update_cam_data(self, *args):
         # Get the selected value from the variable
@@ -35,6 +62,11 @@ class Front_app:
     def get_banner_dir(self):
         self.ban_dir = fd.askdirectory()  # Get the directory from button_1
         #self.button_2.config(state='normal')   Enable button_2 after directory selection
+        if self.ban_dir is not None:
+            self.dateforCatalog1_ = ctk.CTkTextbox(master=self, corner_radius=5, border_color='red', width=300, height=10)
+            self.dateforCatalog1_.place(x=300, y=120, anchor='center')
+            self.dateforCatalog1_.insert('0.0', self.ban_dir)
+            self.dateforCatalog1_.configure(state="disabled")
 
     def resize_banner(self):
         if self.ban_dir is not None and self.baner_size_data is not None:  # Check if a directory is selected
@@ -42,26 +74,14 @@ class Front_app:
             dimensions = self.baner_size_data
             if os.path.exists(f"{self.ban_dir}\\Banner") == False:
                 os.makedirs(f"{self.ban_dir}\\Banner")
+            print(f"To jest to: {self.dateforCatalog_.get('0.0', 'end')}")
+            aps.PhotoshopAPI(self.ban_dir, self.dateforCatalog_.get('0.0', 'end').strip(), int(dimensions)).psBannerResizer()
+            # ptw.convertToWebp(self.ban_dir)
+            # ptw.convertToWebp2(self.ban_dir)
 
-            aps.psBannerResizer(self.ban_dir, self.dateforCatalog_.get(), int(dimensions))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            # sC.connectToServer(f"{self.ban_dir}\\Banner")
+    def quite_app(self):
+        self.quit()
+App()
 
 
