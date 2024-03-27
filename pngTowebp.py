3724dd94b4a024843335d8145fca692aa47b14f6
KamOri1 <142267945+KamOri1@users.noreply.github.com>
2024-03-27 21:31:13 +0100

    Update pngTowebp.py

--- pngTowebp.py
+++ pngTowebp.py
@@ -1,17 +1,14 @@
-from webptools import grant_permission
-from webptools import cwebp
 import os
+from PIL import Image
 
 
-def convertToWebp(catdir):
 
+def convertToWebp2(catdir):
     files = os.listdir(f'{catdir}\\Banner\\')
-    grant_permission()
     for file in files:
-        print(cwebp(input_image=f"{catdir}\\Banner\\{file}", output_image=f"{catdir}\\Banner\\{file.replace('.png', '.webp')}",
-            option="-q 80", logging="-v"))
-        print(f'file: {file}')
-        print(f'file web: {file.replace('.png', '.webp')}')
+        Image.open(f"{catdir}\\Banner\\{file}").save(f"{catdir}\\Banner\\{file.replace('.png', '.webp')}",
+            'webp', lossless=True)
+        print(f'Plik {file} został skonwertowany do .webp')
 
 
 
