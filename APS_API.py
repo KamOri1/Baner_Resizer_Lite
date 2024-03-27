ee024226eea50d2a4dab66309487a5c4747f9cd6
KamOri1 <142267945+KamOri1@users.noreply.github.com>
2024-03-27 21:09:39 +0100

    Update APS_API.py

--- APS_API.py
+++ APS_API.py
@@ -2,45 +2,43 @@ import photoshop.api as ps
 import os
 
 
-def ends_with_del(banner_name):
-    if banner_name.endswith('.jpg'):
-        new_banner_name = banner_name.replace('.jpg', '')
-    elif banner_name.endswith('.png'):
-        new_banner_name = banner_name.replace('.png', '')
-    elif banner_name.endswith('.mp4'):
-        new_banner_name = banner_name.replace('.mp4', '')
-    return new_banner_name
-def psBannerResizer(catDir,data, width_):
-    app = ps.Application()
-    banner_check = list(os.listdir(f"{catDir}"))
-    if width_ == 650:
-        args = [650, 490]
-        banner_format = '_mb'
-    else:
-        args = [610, 242]
-        banner_format = 'b'
-    for banner_name in banner_check:
-        print(banner_name)
-        if banner_name.endswith('.jpg') or banner_name.endswith('.png') or banner_name.endswith('.mp4'):
-        # Load the image file into Photoshop and assign it to the 'a' variable
-            banner_file = app.load(f"{catDir}\\{banner_name}")
-            print('załadowano kraj')
-            # Resize and save the image
-            banner_file.resizeImage(width=args[0], height=args[1], resolution=72, automatic=8)
-            png = f"{catDir}\\Banner\\{ends_with_del(banner_name).lower()}{data}{banner_format}"
-            options = ps.PNGSaveOptions()
-            banner_file.saveAs(png, options, asCopy=True)
-            banner_file.close()
+class PhotoshopAPI:
+    def __init__(self, catDir=None, data=None, width_height=None):
+
+        self.catDir = catDir
+        self.data = data
+        self.width_height = width_height
+
+    def psBannerResizer(self):
+        app = ps.Application()
+        banner_check = list(os.listdir(f"{self.catDir}"))
+        if self.width_height == 650:
+            args = [650, 490]
+            banner_format = '_mb'
         else:
-            continue
+            args = [610, 242]
+            banner_format = 'b'
+        for banner_name in banner_check:
 
+            print(banner_name)
+            if banner_name.endswith('.jpg') or banner_name.endswith('.png') or banner_name.endswith('.mp4'):
+                new_banner_name = banner_name.replace(banner_name[banner_name.index('.')::], '')
+                # Load the image file into Photoshop and assign it to the 'a' variable
+                banner_file = app.load(f"{self.catDir}\\{banner_name}")
+                print('załadowano kraj')
+                # Resize and save the image
+                banner_file.resizeImage(width=args[0], height=args[1], resolution=72, automatic=8)
+                png = f"{self.catDir}\\Banner\\{new_banner_name.lower()}{self.data}{banner_format}"
+                options = ps.PNGSaveOptions()
+                banner_file.saveAs(png, options, asCopy=True)
+                banner_file.close()
+                print(f"zeskalowano {banner_name}")
+            else:
+                continue
 
-
-
-def closePhotoshop(closePS):
-    if closePS.lower() == "close_ps":
-        # zamyka program photoshop
-        ps.Application().quit()
-        print('zakończono')
-
+    def closePhotoshop(self):
+        if closePS.lower() == "close_ps":
+            # zamyka program photoshop
+            ps.Application().quit()
+            print('zakończono')
 
