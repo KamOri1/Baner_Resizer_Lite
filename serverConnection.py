0bc0913dd672fa5007e25e2e9fad022f299cd228
Kamil <kaspian973@interia.pl>
2024-04-12 23:43:44 +0200

    add ServerConnectionAction

--- serverConnection.py
+++ serverConnection.py
@@ -2,6 +2,46 @@ import paramiko
 import os
 from ftplib import FTP
 
+
+class ServerConnectionAction:
+    def __init__(self):
+        ...
+
+    def connectionCheck(self, catDir, ftpOrSftp, hostnameData, usernameData, passwordData, ftpCatDir):
+        self.ftpOrSftp = ftpOrSftp
+        self.hostnameData = hostnameData
+        self.usernameData = usernameData
+        self.passwordData = passwordData
+        self.ftpCatDir = ftpCatDir
+        if self.ftpOrSftp is not None and self.hostnameData is not None and self.usernameData is not None \
+            and self.passwordData is not None and self.ftpCatDir is not None:
+            if self.ftpOrSftp == 'sftp':
+                self.cnopts = paramiko.client.SSHClient()
+                self.cnopts.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+                self.cnopts.connect(hostname=self.hostnameData,
+                               username=self.usernameData,
+                               password=self.passwordData)
+                with self.cnopts.open_sftp() as sftp:
+                    print(f"connected to server: {sftp.getcwd()}")
+                    sftp.chdir(ftpCatDir)
+                    print(f"connected to server 2: {sftp.getcwd()}")
+            elif self.ftpOrSftp == 'ftp':
+                with FTP(host=self.hostnameData,
+                         user=self.usernameData,
+                         passwd=self.passwordData) as ftp:
+                    self.ftp = ftp
+                    print(f"connected to {ftp.pwd()}")
+                    print(ftp.cwd(ftpCatDir))  # Change to `other_dir/
+                    print(f"connected to {ftp.pwd()}")
+
+
+    def disconnection(self):
+        if self.ftpOrSftp == 'sftp':
+            self.cnopts.close()
+        elif self.ftpOrSftp == 'ftp':
+            self.ftp.quit()
+
+
 # self.dateforCatalog_.get('0.0', 'end')
 def connectToServerSFTP(catDir, hostname_, username_, password_, ftpCatDir):
     cnopts = paramiko.client.SSHClient()
@@ -15,17 +55,17 @@ def connectToServerSFTP(catDir, hostname_, username_, password_, ftpCatDir):
 
         sftp.chdir(ftpCatDir)
         print(f"connected to server 2: {sftp.getcwd()}")
-        # current_directory = sftp.getcwd()
-        # if current_directory == ftpCatDir:
-        #     print(f"You are in the {ftpCatDir} directory.")
-        #     checkToSend = list(os.listdir(catDir))
-        #     for ban in checkToSend:
-        #         sftp.put(f"{catDir}\\{ban}", f"{ftpCatDir}//{ban}")
-        #         print(f'Baner {ban} został przesłany na server')
-        #     print(
-        #         "Wszystkie banery zostały przesłane na server #######################################################")
-        # else:
-        #     print(f"{sftp.getcwd()}") # dodać connection error zamiast tego
+        current_directory = sftp.getcwd()
+        if current_directory == ftpCatDir:
+            print(f"You are in the {ftpCatDir} directory.")
+            checkToSend = list(os.listdir(catDir))
+            for ban in checkToSend:
+                sftp.put(f"{catDir}\\{ban}", f"{ftpCatDir}//{ban}")
+                print(f'Baner {ban} został przesłany na server')
+            print(
+                "Wszystkie banery zostały przesłane na server #######################################################")
+        else:
+            print(f"{sftp.getcwd()}") # dodać connection error zamiast tego
         cnopts.close()
 
 
